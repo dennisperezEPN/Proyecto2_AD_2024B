8c086136ae4eef24e68f95efcd6441a11f218916
dennisP <dennis.perez@epn.edu.ec>
2025-01-18 22:28:25 -0500

    Se crean los modelos para mapear las tablas de la BD a clases

---
+++ GestionTareas-backend/Models/Tarea.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GestionTareas_backend.Models
+{
+    public class Tarea
+    {
+        public int id { get; set; }
+        public string descripcion { get; set; }
+        public DateTime fechaCreacion { get; set; }
+        public DateTime fechaPlazo { get; set; }
+        public DateTime? fechaFinalizacion { get; set; }
+        public string prioridad { get; set; } // baja, media, alta
+        public string estado { get; set; } // pendiente, en progreso, completada
+        public string comentarios { get; set; }
+
+        // Foreign Key a Usuario (Relación 1 a N)
+        public int? usuarioId { get; set; } // opcional
+        public Usuario usuario { get; set; }
+    }
+}
---
+++ GestionTareas-backend/Models/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GestionTareas_backend.Models
+{
+    public class Usuario
+    {
+        public int id { get; set; }
+        public string userName { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string perfil { get; set; } // admin o miembro
+
+
+        // Relación 1 a muchos: Un usuario puede tener muchas tareas
+        public ICollection<Tarea> Tareas { get; set; } = new List<Tarea>();
+    }
+}
