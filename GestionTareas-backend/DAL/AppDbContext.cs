779fdcd9023200be74b7e8adedfeb3230d428a93
dennisP <dennis.perez@epn.edu.ec>
2025-01-18 22:24:48 -0500

    Se añade una capa para manejar la conexion con la BD

---
+++ GestionTareas-backend/DAL/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using GestionTareas_backend.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Web;
+
+namespace GestionTareas_backend.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        // Representan tablas de la base de datos
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Tarea> Tareas { get; set; }
+
+    }
+
+}
