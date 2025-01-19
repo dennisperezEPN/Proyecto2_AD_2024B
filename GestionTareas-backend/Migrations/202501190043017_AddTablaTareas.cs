0f6d70a0d7dfd436f4c0053c055128af88acde41
dennisP <dennis.perez@epn.edu.ec>
2025-01-18 22:26:56 -0500

    Se crea migraciones para cambiar la BD

---
+++ GestionTareas-backend/Migrations/202501190043017_AddTablaTareas.cs
@@ -0,0 +1,18 @@
+﻿namespace GestionTareas_backend.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddTablaTareas : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Tareas", "prioridad", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Tareas", "prioridad", c => c.Int(nullable: false));
+        }
+    }
+}
