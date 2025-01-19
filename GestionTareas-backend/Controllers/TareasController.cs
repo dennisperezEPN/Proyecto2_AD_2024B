777e51db445d006ed62a5037c9f5634f484b5a1f
dennisP <dennis.perez@epn.edu.ec>
2025-01-18 22:21:36 -0500

    Se añado los controles para Usuario y Tarea con sus diferentes metodos HTTP y rutas

---
+++ GestionTareas-backend/Controllers/TareasController.cs
@@ -0,0 +1,111 @@
+﻿using GestionTareas_backend.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using GestionTareas_backend.Models;
+using System.Data.Entity;
+
+namespace GestionTareas_backend.Controllers
+{
+    public class TareasController : ApiController
+    {
+        private AppDbContext db = new AppDbContext();
+
+        // Obtener las tareas
+        [HttpGet]
+        public IEnumerable<Tarea> ObtenerTareas()
+        {
+            return db.Tareas.ToList();
+        }
+
+        // Obtener tarea por id
+        [HttpGet]
+        public IHttpActionResult ObtenerTarea(int id)
+        {
+            var tarea = db.Tareas.Find(id);
+
+            if (tarea == null)
+                return NotFound();
+
+            return Ok(tarea);
+        }
+
+        // Filtrar tareas con el estado 'Pendiente'
+        [HttpGet]
+        [Route("api/tareas/filter")]
+        public IHttpActionResult FiltrarTareasPorEstado(string estado)
+        {
+            // Compara el valor del estado de las tareas con el estado
+            // pasado como parametro
+            var tareasFiltradas = db.Tareas
+                .Where(t => t.estado.Equals(estado, StringComparison.OrdinalIgnoreCase))
+                .ToList();
+
+            if (!tareasFiltradas.Any())
+                return NotFound();
+
+            return Ok(tareasFiltradas);
+        }
+
+
+        // Crear una tarea
+        [HttpPost]
+        public IHttpActionResult CrearTarea(Tarea tarea)
+        {
+            // Se comprueba que los datos cumplan con las reglas de 
+            // validacion definidas en el modelo
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            // Se agrega una tarea
+            tarea.fechaCreacion = DateTime.Now;
+            db.Tareas.Add(tarea);
+            db.SaveChanges();
+
+            // Devuelve una respuesta HTTP 201 Created
+            // Incluye el recurso creado
+            return CreatedAtRoute("DefaultApi", new { id = tarea.id }, tarea);
+
+
+        }
+
+        // Actualizar una tarea
+        [HttpPut]
+        public IHttpActionResult ActualizarTarea(int id, Tarea tarea)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            // Se actualiza el recurso
+            db.Entry(tarea).State = EntityState.Modified;
+            db.SaveChangesAsync();
+
+            // Devuelve una respuesta HTTP 204 No Content.
+            // Indica que no hay nada que devolver en el cuerpo
+            // de la respuesta
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // Eliminar una tarea
+        [HttpDelete]
+        public IHttpActionResult EliminarTarea(int id)
+        {
+            var tarea = db.Tareas.Find(id);
+
+            if (tarea == null)
+                return NotFound();
+
+            db.Tareas.Remove(tarea);
+            db.SaveChanges();
+
+            return Ok(tarea);
+        }
+    }
+}
---
+++ GestionTareas-backend/Controllers/UsuariosController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.Entity;
+using GestionTareas_backend.DAL;
+using GestionTareas_backend.Models;
+
+
+namespace GestionTareas_backend.Controllers
+{
+    public class UsuariosController : ApiController
+    {
+        private AppDbContext bd = new AppDbContext();
+
+        // Solicitudes para obtener usuarios
+        [HttpGet]
+        public IEnumerable<Usuario> ObtenerUsuarios()
+        {
+            return bd.Usuarios.ToList();
+        }
+
+        [HttpGet]
+        public IHttpActionResult ObtenerUsuario(int id)
+        {
+            var usuario = bd.Usuarios.Find(id);
+
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(usuario);
+        }
+
+        // Solicitud para crear un usuario
+        [HttpPost]
+        public IHttpActionResult CrearUsuario(Usuario usuario)
+        {
+            // Se comprueba que los datos cumplan con las reglas de 
+            // validacion definidas en el modelo
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            // Se agrega el usuario
+            bd.Usuarios.Add(usuario);
+            bd.SaveChanges();
+
+            // Devuelve una respuesta HTTP 201 Created
+            // Incluye el recurso creado
+            return CreatedAtRoute("DefaultApi", new {id = usuario.id}, usuario);
+        }
+
+        // Solicitud para modificar un usuario
+        [HttpPut]
+        public IHttpActionResult ActualizarUsuario(int id, Usuario usuario)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            // Comprueba que el id de la ruta de la solicitud, coincida con
+            // id del objeto enviado en el cuerpo
+            if (id != usuario.id)
+                return BadRequest();
+
+            // Se actualiza el recurso
+            bd.Entry(usuario).State = EntityState.Modified;
+            bd.SaveChangesAsync();
+
+            // Devuelve una respuesta HTTP 204 No Content.
+            // Indica que no hay nada que devolver en el cuerpo
+            // de la respuesta
+            return StatusCode(HttpStatusCode.NoContent);
+
+        }
+
+        // Solicitud para borrar un usuario
+        [HttpDelete]
+        public IHttpActionResult EliminarUsuario (int id)
+        {
+            var usuario = bd.Usuarios.Find(id);
+
+            if (usuario == null)
+                return NotFound();
+
+            bd.Usuarios.Remove(usuario);
+            bd.SaveChanges();
+            
+            return Ok(usuario);
+        }
+    }
+}
